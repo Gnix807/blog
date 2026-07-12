a0756011aad6d4f83eedeb85c696a09654528663
Gnix807 <1548398780@qq.com>
2026-07-12 15:15:06 +0800

    网站字体替换：正文→MiSans Latin VF，标题→Montserrat，大号描边→Barlow Condensed，移除抖音美好体CDN

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,8 +34,6 @@ export default defineNuxtConfig({
 				// "JetBrains Mono", 思源宋体 "Noto Serif SC"
 				{ rel: 'preconnect', href: 'https://fonts.gstatic.cn', crossorigin: '' },
 				{ rel: 'stylesheet', href: 'https://fonts.googleapis.cn/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Noto+Serif+SC:wght@200..900&display=swap', media: 'print', onload: 'this.media="all"' },
-				// 抖音美好体 "DOUYINSANSBOLD-GB"
-				{ rel: 'stylesheet', href: 'https://fonts.bytedance.com/dfd/api/v1/css?family=DOUYINSANSBOLD-GB&display=swap', media: 'print', onload: 'this.media="all"' },
 			],
 			templateParams: {
 				separator: '|',
